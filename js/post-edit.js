8d99342f09da6d43de988415aa6ad066c96257fa
daryabono <darya.g@bono.digital>
2024-04-09 01:02:19 +0300

    module9-task2

---
+++ js/post-edit.js
@@ -0,0 +1,41 @@
+const MAX_VALUE = 100;
+const MIN_VALUE = 25;
+const SCALE_STEP = 25;
+const scaleSmallerButtonElement = document.querySelector('.scale__control--smaller');
+const scaleBiggerButtonElement = document.querySelector('.scale__control--bigger');
+const scaleValueElement = document.querySelector('.scale__control--value');
+const imagePreviewElement = document.querySelector('.img-upload__preview').querySelector('img');
+
+let currentValue = parseFloat(scaleValueElement.value);
+
+const scaleBiggerImage = () => {
+  scaleValueElement.value = `${currentValue += SCALE_STEP}%`;
+  imagePreviewElement.style.transform = `scale(${parseFloat(scaleValueElement.value) / 100})`;
+};
+
+const scaleSmallerImage = () => {
+  scaleValueElement.value = `${currentValue -= SCALE_STEP}%`;
+  imagePreviewElement.style.transform = `scale(${parseFloat(scaleValueElement.value) / 100})`;
+};
+
+const scaleBiggerControl = () => {
+  if (currentValue === MAX_VALUE) {
+    return false;
+  }
+
+  scaleBiggerImage();
+};
+
+const scaleSmallerControl = () => {
+  if (currentValue === MIN_VALUE) {
+    return false;
+  }
+
+  scaleSmallerImage();
+};
+
+scaleBiggerButtonElement.addEventListener('click', scaleBiggerControl);
+
+scaleSmallerButtonElement.addEventListener('click', scaleSmallerControl);
+
+export {imagePreviewElement};
