332ae604b3c08113b939dfd261cda4b72d20e4b2
daryabono <darya.g@bono.digital>
2024-04-09 23:57:23 +0300

    module12-task2

--- js/post-upload.js
+++ js/post-upload.js
@@ -1,12 +1,25 @@
 import { isEscapeKey } from './util.js';
-import { scaleReset } from './post-edit.js';
+import { imagePreviewElement, scaleReset } from './post-edit.js';
 import { filterReset } from './post-filter.js';
+
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
 const imageUploadFormElement = document.querySelector('.img-upload__form');
 const fileUploadControlElement = document.querySelector('#upload-file');
 const imageUploadOverlayElement = document.querySelector('.img-upload__overlay');
 const fileUploadCloseButtonElement = document.querySelector('#upload-cancel');
 const inputsElements = document.querySelectorAll('input');
+
+fileUploadControlElement.addEventListener('change', () => {
+  const file = fileUploadControlElement.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const fileTypeMatches = FILE_TYPES.some((end) => fileName.endsWith(end));
+
+  if (fileTypeMatches) {
+    imagePreviewElement.src = URL.createObjectURL(file);
+  }
+});
 
 const onFileUploadOpen = () => {
   imageUploadOverlayElement.classList.remove('hidden');
